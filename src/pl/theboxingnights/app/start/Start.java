5625d3f48bca0bb351bcf31cc3793cecd7774e60
dziku <fenix_91@go2.pl>
2015-06-05 03:14:53 +0200

    Dodałem metodę simpleInitApp

--- src/pl/theboxingnights/app/start/Start.java
+++ src/pl/theboxingnights/app/start/Start.java
@@ -1,8 +1,14 @@
 package pl.theboxingnights.app.start;
 
+import com.jme3.app.SimpleApplication;
+
 /**
  * Created by filip on 05.06.15.
  */
-public class Start {
-    
+public class Start extends SimpleApplication {
+
+    @Override
+    public void simpleInitApp() {
+
+    }
 }
