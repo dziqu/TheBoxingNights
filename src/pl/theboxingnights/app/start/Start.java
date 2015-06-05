aec61baa811409f79368af97fd3c36a2feeb04be
dziku <fenix_91@go2.pl>
2015-06-05 04:43:45 +0200

    Dodałem wywołania klasy DisplaySettings

--- src/pl/theboxingnights/app/start/Start.java
+++ src/pl/theboxingnights/app/start/Start.java
@@ -1,15 +1,30 @@
 package pl.theboxingnights.app.start;
 
 import com.jme3.app.SimpleApplication;
+import pl.theboxingnights.app.settings.DisplaySettings;
+
+import java.util.prefs.BackingStoreException;
 
 /**
  * Created by filip on 05.06.15.
  */
 public class Start extends SimpleApplication {
 
-    public static void main (String [] args) {
+    public Start () throws BackingStoreException {
+        initDisplaySettings();
+    }
+
+    public static void main (String [] args) throws BackingStoreException {
         Start app = new Start();
         app.start();
+    }
+
+    private void initDisplaySettings() throws BackingStoreException {
+        DisplaySettings displaySettings = new DisplaySettings(this);
+        displaySettings.load();
+        displaySettings.getSettings();
+        displaySettings.save();
+        displaySettings.setSettings();
     }
 
 
@@ -17,4 +32,5 @@ public class Start extends SimpleApplication {
     public void simpleInitApp() {
 
     }
+
 }
